8c78fa00be65effaed2d154b19ecff564c8f1510
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2022-04-18 01:46:38 +0530

    add simple routing to the app

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import routes from "../routes";
 import NoMatch from "./NoMatch";
 
 const App = () => {
   return (
-    <>
+    <Switch>
       {routes.map((route) => (
-        <Route key={route.path} path={route.path} component={route.component} />
+        <Route
+          key={route.path}
+          path={route.path}
+          exact
+          component={route.component}
+        />
       ))}
       <Route path="*" component={NoMatch} />
-    </>
+    </Switch>
   );
 };
 
